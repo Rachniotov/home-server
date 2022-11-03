de81c183400152295c951c7ec686e537a017937c
Rachniotov <71971848+Rachniotov@users.noreply.github.com>
2022-11-03 21:29:02 +0530

    add endpoint for intruder alert

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
 const express = require("express");
 const app = express();
-const TeachableMachine = require("@sashido/teachablemachine-node");
-
-const model = new TeachableMachine({
-  modelUrl: ""	// model link here
-});
 
 const port = 5999;
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('hi')
+  res.send('hi');
 });
 
-app.get("/face-rec", (req, res) => {
-	model.classify({
-		imageUrl: req.body.img,
-	  }).then((predictions) => {
-		res.send(predictions)
-	  }).catch((e) => {
-		console.log("ERROR", e);
-	});
+app.get('/door-intruder', (req, res) => {
+	console.log("ATTENTION!! INTRUDER DETECTED");
 });
 
 app.listen(port, () => {
-  console.log(`app listening on port ${port}`)
+  console.log(`app listening on port ${port}`);
 });
 
